e054cf30121c3a9b6994d17cc3013718cda044b4
TheBlackEntity <36412486+TheBlackEntity@users.noreply.github.com>
2023-07-28 16:56:49 +0200

    Remove scary message

--- src/main/java/me/entity303/plugmanbungee/main/PlugManBungee.java
+++ src/main/java/me/entity303/plugmanbungee/main/PlugManBungee.java
@@ -6,8 +6,6 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 
-import java.util.concurrent.TimeUnit;
-
 public final class PlugManBungee extends Plugin implements Listener {
     private static PlugManBungee instance;
 
@@ -17,15 +15,6 @@ public final class PlugManBungee extends Plugin implements Listener {
 
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new PluginsCommand());
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new PlugManBungeeCommand());
-
-        for (int i = 1; i < 4; i++) {
-            ProxyServer.getInstance().getScheduler().schedule(this, () -> {
-                getLogger().severe("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                getLogger().severe("This version of PlugMan is still in very *alpha*, please do not expect it to work perfectly!");
-                getLogger().severe("Also do not expect for every feature to be implemented!");
-                getLogger().severe("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            }, i, TimeUnit.SECONDS);
-        }
     }
 
     @Override
